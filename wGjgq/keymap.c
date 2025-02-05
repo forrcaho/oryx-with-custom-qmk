01b82c1de419d80f1f203c61a96b35b06c70ea99
Forrest Cahoon <forrest.cahoon@gmail.com>
2025-02-05 15:12:38 -0600

    fix missing curly bracket

--- wGjgq/keymap.c
+++ wGjgq/keymap.c
@@ -10,52 +10,48 @@
 // This is the half cycle duration for caps word blink.
 #define CAPS_WORD_BLINK_MS 275
 
-#define LAYER0_HSV \
-    { 152, 255, 255 }
+#define LAYER0_HSV {152, 255, 255}
 
-#define LAYER1_HSV \
-    { 188, 255, 255 }
+#define LAYER1_HSV {188, 255, 255}
 
-#define LAYER2_HSV \
-    { 74, 255, 255 }
+#define LAYER2_HSV {74, 255, 255}
 
-#define LAYER3_HSV \
-    { 41, 255, 255 }
+#define LAYER3_HSV {41, 255, 255}
 
 #define LAYER1_KEY 29
 #define LAYER2L_KEY 33
 #define LAYER2R_KEY 69
 #define LAYER3_KEY 65
 
-#define SET_LAYER_KEY(LAYER, KEY, VALUES)   \
-    {                                       \
-        const uint8_t values[] = VALUES;    \
-        ledmap[LAYER][KEY][0]  = values[0]; \
-        ledmap[LAYER][KEY][1]  = values[1]; \
-        ledmap[LAYER][KEY][2]  = values[2]; \
-    }
+#define SET_LAYER_KEY(LAYER, KEY, VALUES)                                      \
+  {                                                                            \
+    const uint8_t values[] = VALUES;                                           \
+    ledmap[LAYER][KEY][0] = values[0];                                         \
+    ledmap[LAYER][KEY][1] = values[1];                                         \
+    ledmap[LAYER][KEY][2] = values[2];                                         \
+  }
 
-  enum custom_keycodes {
+enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
-    /* fc-added */
-    PG_AMP,
-    PG_ARRO,
-    PG_ENDC,
-    PG_EQEQ,
-    PG_EQL,
-    PG_LBRK,
-    PG_LPAR,
-    PG_MINS,
-    PG_NEEQ,
-    PG_QMRK,
-    PG_RBRK,
-    PG_RPAR,
-    PG_SLAS,
-    PG_VBAR,
-    /* end fc-added */
-  };
+  /* fc-added */
+  PG_AMP,
+  PG_ARRO,
+  PG_ENDC,
+  PG_EQEQ,
+  PG_EQL,
+  PG_LBRK,
+  PG_LPAR,
+  PG_MINS,
+  PG_NEEQ,
+  PG_QMRK,
+  PG_RBRK,
+  PG_RPAR,
+  PG_SLAS,
+  PG_VBAR,
+  /* end fc-added */
+};
 
-
+// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
     KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           TG(1),                                          TG(3),          KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
@@ -90,12 +86,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
 };
+// clang-format on
 
 extern rgb_config_t rgb_matrix_config;
 
-void keyboard_post_init_user(void) {
-  rgb_matrix_enable();
-}
+void keyboard_post_init_user(void) { rgb_matrix_enable(); }
 
 // clang-format off
 uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
@@ -140,54 +135,56 @@ uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
     {0,0,0}, {0,0,0}
   },
 };
-// clang-format on;
+// clang-format on
 
 void set_layer_color(int layer) {
   for (int i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
     HSV hsv = {
-      .h = pgm_read_byte(&ledmap[layer][i][0]),
-      .s = pgm_read_byte(&ledmap[layer][i][1]),
-      .v = pgm_read_byte(&ledmap[layer][i][2]),
+        .h = pgm_read_byte(&ledmap[layer][i][0]),
+        .s = pgm_read_byte(&ledmap[layer][i][1]),
+        .v = pgm_read_byte(&ledmap[layer][i][2]),
     };
     if (!hsv.h && !hsv.s && !hsv.v) {
-        rgb_matrix_set_color( i, 0, 0, 0 );
+      rgb_matrix_set_color(i, 0, 0, 0);
     } else {
-        RGB rgb = hsv_to_rgb( hsv );
-        float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
-        rgb_matrix_set_color( i, f * rgb.r, f * rgb.g, f * rgb.b );   
+      RGB rgb = hsv_to_rgb(hsv);
+      float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
+      rgb_matrix_set_color(i, f * rgb.r, f * rgb.g, f * rgb.b);
     }
   }
 }
 
 bool rgb_matrix_indicators_user(void) {
   if (rawhid_state.rgb_control) {
-      return false;
+    return false;
   }
-  if (keyboard_config.disable_layer_led) { return false; }
+  if (keyboard_config.disable_layer_led) {
+    return false;
+  }
   switch (biton32(layer_state)) {
-    case 0:
+  case 0:
     /* fc-added */
     SET_LAYER_KEY(2, LAYER2L_KEY, LAYER0_HSV)
     SET_LAYER_KEY(2, LAYER2R_KEY, LAYER0_HSV)
     SET_LAYER_KEY(3, LAYER3_KEY, LAYER0_HSV)
     /* end fc-added */
     set_layer_color(0);
-      break;
-    case 1:
+    break;
+  case 1:
     /* fc-added */
     SET_LAYER_KEY(2, LAYER2L_KEY, LAYER1_HSV)
     SET_LAYER_KEY(2, LAYER2R_KEY, LAYER1_HSV)
     SET_LAYER_KEY(3, LAYER3_KEY, LAYER1_HSV)
     /* end fc-added */
     set_layer_color(1);
-      break;
-    case 2:
-      set_layer_color(2);
-      break;
-    case 3:
-      set_layer_color(3);
-      break;
-   default:
+    break;
+  case 2:
+    set_layer_color(2);
+    break;
+  case 3:
+    set_layer_color(3);
+    break;
+  default:
     if (rgb_matrix_get_flags() == LED_FLAG_NONE)
       rgb_matrix_set_color_all(0, 0, 0);
     break;
@@ -196,20 +193,30 @@ bool rgb_matrix_indicators_user(void) {
 }
 
 /* fc-added */
-const key_override_t pg_lbrk_none  = ko_make_with_layers_and_negmods(0, PG_LBRK, KC_LEFT_CURLY_BRACE, ~0, ~0);
-const key_override_t pg_lbrk_shift = ko_make_with_layers_and_negmods(MOD_MASK_SHIFT, PG_LBRK, KC_LEFT_BRACKET, ~0, ~MOD_MASK_SHIFT);
+const key_override_t pg_lbrk_none =
+    ko_make_with_layers_and_negmods(0, PG_LBRK, KC_LEFT_CURLY_BRACE, ~0, ~0);
+const key_override_t pg_lbrk_shift = ko_make_with_layers_and_negmods(
+    MOD_MASK_SHIFT, PG_LBRK, KC_LEFT_BRACKET, ~0, ~MOD_MASK_SHIFT);
 
-const key_override_t pg_lpar_none  = ko_make_with_layers_and_negmods(0, PG_LPAR, KC_LEFT_PAREN, ~0, ~0);
-const key_override_t pg_lpar_shift = ko_make_with_layers_and_negmods(MOD_MASK_SHIFT, PG_LPAR, KC_LEFT_ANGLE_BRACKET, ~0, ~MOD_MASK_SHIFT);
+const key_override_t pg_lpar_none =
+    ko_make_with_layers_and_negmods(0, PG_LPAR, KC_LEFT_PAREN, ~0, ~0);
+const key_override_t pg_lpar_shift = ko_make_with_layers_and_negmods(
+    MOD_MASK_SHIFT, PG_LPAR, KC_LEFT_ANGLE_BRACKET, ~0, ~MOD_MASK_SHIFT);
 
-const key_override_t pg_rbrk_none  = ko_make_with_layers_and_negmods(0, PG_RBRK, KC_RIGHT_CURLY_BRACE, ~0, ~0);
-const key_override_t pg_rbrk_shift = ko_make_with_layers_and_negmods(MOD_MASK_SHIFT, PG_RBRK, KC_RIGHT_BRACKET, ~0, ~MOD_MASK_SHIFT);
+const key_override_t pg_rbrk_none =
+    ko_make_with_layers_and_negmods(0, PG_RBRK, KC_RIGHT_CURLY_BRACE, ~0, ~0);
+const key_override_t pg_rbrk_shift = ko_make_with_layers_and_negmods(
+    MOD_MASK_SHIFT, PG_RBRK, KC_RIGHT_BRACKET, ~0, ~MOD_MASK_SHIFT);
 
-const key_override_t pg_rpar_none  = ko_make_with_layers_and_negmods(0, PG_RPAR, KC_RIGHT_PAREN, ~0, ~0);
-const key_override_t pg_rpar_shift = ko_make_with_layers_and_negmods(MOD_MASK_SHIFT, PG_RPAR, KC_RIGHT_ANGLE_BRACKET, ~0, ~MOD_MASK_SHIFT);
+const key_override_t pg_rpar_none =
+    ko_make_with_layers_and_negmods(0, PG_RPAR, KC_RIGHT_PAREN, ~0, ~0);
+const key_override_t pg_rpar_shift = ko_make_with_layers_and_negmods(
+    MOD_MASK_SHIFT, PG_RPAR, KC_RIGHT_ANGLE_BRACKET, ~0, ~MOD_MASK_SHIFT);
 
-const key_override_t pg_slas_none  = ko_make_with_layers_and_negmods(0, PG_SLAS, KC_SLASH, ~0, ~0);
-const key_override_t pg_slas_shift = ko_make_with_layers_and_negmods(MOD_MASK_SHIFT, PG_SLAS, KC_BACKSLASH, ~0, ~MOD_MASK_SHIFT);
+const key_override_t pg_slas_none =
+    ko_make_with_layers_and_negmods(0, PG_SLAS, KC_SLASH, ~0, ~0);
+const key_override_t pg_slas_shift = ko_make_with_layers_and_negmods(
+    MOD_MASK_SHIFT, PG_SLAS, KC_BACKSLASH, ~0, ~MOD_MASK_SHIFT);
 
 // clang-format off
 const key_override_t **key_overrides = (const key_override_t *[]){
@@ -224,20 +231,21 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   /* fc-added */
-  uint8_t std_mods     = get_mods();
+  uint8_t std_mods = get_mods();
   uint8_t oneshot_mods = get_oneshot_mods();
-  uint8_t all_mods     = std_mods | oneshot_mods;
-  bool    shift_on     = all_mods & MOD_MASK_SHIFT;
-  bool    ctrl_on      = all_mods & MOD_MASK_CTRL;
+  uint8_t all_mods = std_mods | oneshot_mods;
+  bool shift_on = all_mods & MOD_MASK_SHIFT;
+  bool ctrl_on = all_mods & MOD_MASK_CTRL;
 
   // None of our special keycodes use ALT or GUI mods (so far), so we
   // check once here at the top and let other code handle it if any of
   // these are set.
-  if (all_mods & (MOD_MASK_ALT | MOD_MASK_GUI)) return true;
+  if (all_mods & (MOD_MASK_ALT | MOD_MASK_GUI))
+    return true;
   /* end fc-added */
   switch (keycode) {
-    
-    case RGB_SLD:
+
+  case RGB_SLD:
     if (rawhid_state.rgb_control) {
       return false;
     }
@@ -247,7 +255,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return false;
     /* fc-added */
     break;
-    case KC_CAPS:
+  case KC_CAPS:
     // On caps lock release, if caps lock just got turned off, ensure that
     // backlight is enabled when it stops blinking.
     if (!record->event.pressed) {
@@ -258,145 +266,181 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
     }
     break;
-    // Macros
-    case PG_AMP:
+  // Macros
+  case PG_AMP:
     if (record->event.pressed) {
       clear_mods();
       clear_oneshot_mods();
       if (shift_on) {
-        if (ctrl_on) SEND_STRING("&");
-        else SEND_STRING(" & ");
+        if (ctrl_on)
+          SEND_STRING("&");
+        else
+          SEND_STRING(" & ");
       } else {
-        if (ctrl_on) SEND_STRING("&&");
-        else SEND_STRING(" && ");
+        if (ctrl_on)
+          SEND_STRING("&&");
+        else
+          SEND_STRING(" && ");
       }
       set_mods(std_mods);
       set_oneshot_mods(oneshot_mods);
       return false;
     }
     break;
-    case PG_ARRO:
+  case PG_ARRO:
     if (record->event.pressed) {
       clear_mods();
       clear_oneshot_mods();
       if (shift_on) {
-        if (ctrl_on) SEND_STRING("->");
-        else SEND_STRING(" -> ");
+        if (ctrl_on)
+          SEND_STRING("->");
+        else
+          SEND_STRING(" -> ");
       } else {
-        if (ctrl_on) SEND_STRING("=>");
-        else SEND_STRING(" => ");
+        if (ctrl_on)
+          SEND_STRING("=>");
+        else
+          SEND_STRING(" => ");
       }
       set_mods(std_mods);
       set_oneshot_mods(oneshot_mods);
       return false;
     }
     break;
-    case PG_ENDC:
+  case PG_ENDC:
     if (record->event.pressed) {
       clear_mods();
       clear_oneshot_mods();
       if (shift_on) {
-        if (ctrl_on) SEND_STRING(SS_TAP(X_END));
-        else SEND_STRING(SS_TAP(X_END)SS_TAP(X_ENTER));
+        if (ctrl_on)
+          SEND_STRING(SS_TAP(X_END));
+        else
+          SEND_STRING(SS_TAP(X_END) SS_TAP(X_ENTER));
       } else {
-        if (ctrl_on) SEND_STRING(SS_TAP(X_END)";");
-        else SEND_STRING(SS_TAP(X_END)";"SS_TAP(X_ENTER));
+        if (ctrl_on)
+          SEND_STRING(SS_TAP(X_END) ";");
+        else
+          SEND_STRING(SS_TAP(X_END) ";" SS_TAP(X_ENTER));
       }
       set_mods(std_mods);
       set_oneshot_mods(oneshot_mods);
       return false;
     }
     break;
-    case PG_EQEQ:
+  case PG_EQEQ:
     if (record->event.pressed) {
       clear_mods();
       clear_oneshot_mods();
       if (shift_on) {
-        if (ctrl_on) SEND_STRING("==");
-        else SEND_STRING(" == ");
+        if (ctrl_on)
+          SEND_STRING("==");
+        else
+          SEND_STRING(" == ");
       } else {
-        if (ctrl_on) SEND_STRING("==");
-        else SEND_STRING(" === ");
+        if (ctrl_on)
+          SEND_STRING("==");
+        else
+          SEND_STRING(" === ");
       }
       set_mods(std_mods);
       set_oneshot_mods(oneshot_mods);
       return false;
     }
     break;
-    case PG_EQL:
+  case PG_EQL:
     if (record->event.pressed) {
       clear_mods();
       clear_oneshot_mods();
       if (shift_on) {
-        if (ctrl_on) SEND_STRING("+");
-        else SEND_STRING(" + ");
+        if (ctrl_on)
+          SEND_STRING("+");
+        else
+          SEND_STRING(" + ");
       } else {
-        if (ctrl_on) SEND_STRING("=");
-        else SEND_STRING(" = ");
-      }
-      set_mods(std_mods);
-      set_oneshot_mods(oneshot_mods);
-      return false;
-    
-    break;
-    case PG_MINS:
-    if (record->event.pressed) {
-      clear_mods();
-      clear_oneshot_mods();
-      if (shift_on) {
-        if (ctrl_on) SEND_STRING("-");
-        else SEND_STRING(" - ");
-      } else {
-        if (ctrl_on) SEND_STRING("*");
-        else SEND_STRING(" * ");
+        if (ctrl_on)
+          SEND_STRING("=");
+        else
+          SEND_STRING(" = ");
       }
       set_mods(std_mods);
       set_oneshot_mods(oneshot_mods);
       return false;
     }
     break;
-    case PG_NEEQ:
+  case PG_MINS:
     if (record->event.pressed) {
       clear_mods();
       clear_oneshot_mods();
       if (shift_on) {
-        if (ctrl_on) SEND_STRING("!=");
-        else SEND_STRING(" != ");
+        if (ctrl_on)
+          SEND_STRING("-");
+        else
+          SEND_STRING(" - ");
       } else {
-        if (ctrl_on) SEND_STRING("!==");
-        else SEND_STRING(" !== ");
+        if (ctrl_on)
+          SEND_STRING("*");
+        else
+          SEND_STRING(" * ");
       }
       set_mods(std_mods);
       set_oneshot_mods(oneshot_mods);
       return false;
     }
     break;
-    case PG_QMRK:
+  case PG_NEEQ:
     if (record->event.pressed) {
       clear_mods();
       clear_oneshot_mods();
       if (shift_on) {
-        if (ctrl_on) SEND_STRING("?");
-        else SEND_STRING(" ? ");
+        if (ctrl_on)
+          SEND_STRING("!=");
+        else
+          SEND_STRING(" != ");
       } else {
-        if (ctrl_on) SEND_STRING("??");
-        else SEND_STRING(" ?? ");
+        if (ctrl_on)
+          SEND_STRING("!==");
+        else
+          SEND_STRING(" !== ");
       }
       set_mods(std_mods);
       set_oneshot_mods(oneshot_mods);
       return false;
     }
     break;
-    case PG_VBAR:
+  case PG_QMRK:
     if (record->event.pressed) {
       clear_mods();
       clear_oneshot_mods();
       if (shift_on) {
-        if (ctrl_on) SEND_STRING("|");
-        else SEND_STRING(" | ");
+        if (ctrl_on)
+          SEND_STRING("?");
+        else
+          SEND_STRING(" ? ");
       } else {
-        if (ctrl_on) SEND_STRING("||");
-        else SEND_STRING(" || ");
+        if (ctrl_on)
+          SEND_STRING("??");
+        else
+          SEND_STRING(" ?? ");
+      }
+      set_mods(std_mods);
+      set_oneshot_mods(oneshot_mods);
+      return false;
+    }
+    break;
+  case PG_VBAR:
+    if (record->event.pressed) {
+      clear_mods();
+      clear_oneshot_mods();
+      if (shift_on) {
+        if (ctrl_on)
+          SEND_STRING("|");
+        else
+          SEND_STRING(" | ");
+      } else {
+        if (ctrl_on)
+          SEND_STRING("||");
+        else
+          SEND_STRING(" || ");
       }
       set_mods(std_mods);
       set_oneshot_mods(oneshot_mods);
